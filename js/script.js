b73946e0d00a1987dcd254052ae3d8b769febbc5
Thomas Dodson <tw.dodson@yahoo.co.uk>
2018-11-20 23:45:20 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('p').click(function() {
+	$(this).slideToggle('slow');
+});
+
+$('button').mouseenter(function(){
+    $(this).removeClass('makeRed').addClass('makeBlue');
+});
+
+$('button').mouseleave(function(){
+    $(this).removeClass('makeBlue').addClass('makeRed');
+});
+
